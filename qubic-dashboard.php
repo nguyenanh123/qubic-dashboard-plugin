8d9b325ea90e0d2a8675324b34a4b0e226761795
nguyenanh123 <nguyenanh123@gmail.com>
2026-03-15 21:38:49 +0700

    Create main plugin file qubic-dashboard.php

---
+++ qubic-dashboard.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Qubic Dashboard
+ * Description: This is the main plugin file for the Qubic Dashboard plugin.
+ * Version: 1.0
+ * Author: Your Name
+ */
+
+// Main functionality goes here
+
